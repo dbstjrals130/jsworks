ac76102ae6617c2187d8cd59a2b54da8dbc15566
seokmin1 <dbstjrals130@naver.com>
2023-11-10 18:10:20 +0900

    복습하기

---
+++ day3/js/array2.js
@@ -0,0 +1,49 @@
+//Array 객체를 사용한 배열
+let num = new Array();  //빈 배열 생성
+console.log(num);
+console.log(num.length + "개");
+
+//저장(입력)
+num[0] = 10;
+num[1] = 20;
+num[2] = 30;
+
+console.log(num);
+
+//특정 요소 1개 조회
+console.log(num[0]);
+
+//전체 요소 검색
+for(let i=0; i<num.length; i++) {
+  console.log(num[i]);
+}
+
+//for in문 사용
+/* for(let 변수 in 배열이름) {
+  console.log(배열이름[변수])
+} */
+console.log("for ~ in문 검색")
+for(let n in num) {
+  console.log(num[n]);
+}
+
+
+for(let i in animal) {
+  document.write(animal[i]);
+}
+
+//연산
+console.log("인덱스로 연산하기");
+console.log(num[1] - num[2]);
+console.log(num[1] <= num[2]);
+
+//합계 및 평균
+let sum = 0;
+let avg;
+for(let i=0; i<num.length; i++) {
+  sum = sum + num[i];
+  sum += num[i];  //누적 합계
+}
+avg = sum / num.length;   //평균 = 합계 / 개수
+console.log(sum);
+console.log(avg);
---
+++ day3/js/function_return.js
@@ -0,0 +1,22 @@
+//return이 있는 함수
+
+//제곱수를 계산하는 함수
+//어떤수를 입력받아 제곱하는 함수
+function square(x){
+  let num = x * x;
+}
+
+/* function add(x, y){
+  return x + y;   //구문 리턴
+} */
+
+let add = function(x, y){
+  return x + y;
+}
+
+console.log(result);
+
+let result = add(10, 11)
+console.log(result);
+//console.log(add(4, 5));
+
